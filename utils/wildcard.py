d4170aac7b11bf6bea45d3a074457a0cf454030a
Zenoix <41812358+Zenoix@users.noreply.github.com>
2022-03-10 17:09:27 +1300

    Added docstrings and dunder str and repr methods

--- utils/wildcard.py
+++ utils/wildcard.py
@@ -1,3 +1,17 @@
 class WildCard:
-    def __eq__(self, _):
+    """Wildcard to be used as an 'any character' character."""
+    def __eq__(self, _: str) -> bool:
+        """
+        As a wildcard character, it returns True
+        when compared to any character.
+
+        Arguments:
+        _ -- The other character
+        """
         return True
+
+    def __str__(self) -> str:
+        return "'_'"
+
+    def __repr__(self) -> str:
+        return "'_'"
